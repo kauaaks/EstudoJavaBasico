158ffd1a13c034761226e636726e7f3138bb8d29
Kauã Aksenow <kaua.mota2@etec.sp.gov.br>
2025-07-04 13:43:02 -0300

    Create FuncoesMath.java

---
+++ FuncoesMath.java
@@ -0,0 +1,36 @@
+//funções matemáticas em java
+public class Main {
+    public static void main(String[] args) {
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+        //Math.sqrt -> raiz quadrada.
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.println("Raiz quadrada de " + x + " = " + A);
+        System.out.println("Raiz quadrada de " + y + " = " + B);
+        System.out.println("Raiz quadrada de 25 = " + C);
+        //Math.pow -> potência, x elevado a y.
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+        // Math.abs -> valor absoluto/módulo.
+        A = Math.abs(y);
+        B = Math.abs(z);
+        System.out.println("Valor absoluto de " + y + " = " + A);
+        System.out.println("Valor absoluto de " + z + " = " + B);
+        //formula de delta, x1 e x2 de uma equação de segundo grau.
+        double a, b, c, delta, x1, x2;
+        a = 2;
+        b = 3;
+        c = 4;
+        delta = Math.pow(b, 2.0) - 4 * a * c;
+        x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+    }
+}
